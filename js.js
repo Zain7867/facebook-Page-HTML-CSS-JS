a4c86745debaf541aec297d61815a62692c05e33
Zain7867 <zainmansuri7@gmail.com>
2019-03-11 23:51:01 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+
+function myClick() {
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    alert("Email: "+ email + "Password : "+pass);
+    console.log(email,pass);
+}
